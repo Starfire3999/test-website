f630b01eed6bb1fd74fd592d76593f804922925b
Yolandie <starfire3999@gmail.com>
2022-08-21 14:35:37 +0200

    new jscipt added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-function buttonClicked() {
-    alert ('I AM ClICKED') ;
 
-}
+$(function(){
+    var pages = ['index', 'about', 'contact'];
+    var pathname = window.location.pathname;
 
-function buttonToChangeTextClicked(){
-    document.getElementById('targetTag').innerHTML = "JavaScript Text";
-
-}
+    $('.nav-link').each(function(i){
+        if(pathname.includes(pages[i])){
+            $(this).addClass('active');
+            $(this).attr('aria-current', 'page');
+        }else if(this.className.includes('active')){
+            $(this).removeClass('active');
+        }
+    });
+});
